b48fa310153b5a526f0bb411c1e4d6e66e4af2b9
msd.khoshkam <msd.khoshkam@gmail.com>
2019-09-30 17:21:29 +0330

    Update

--- example/lib/NativeBannerPage.dart
+++ example/lib/NativeBannerPage.dart
@@ -3,6 +3,8 @@ import 'package:tapsell_plus/tapsell_plus.dart';
 import 'package:tapsell_plus_example/Constant.dart';
 
 class NativeBannerPage extends StatelessWidget {
+  String adImage, adTitle;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +21,18 @@ class NativeBannerPage extends StatelessWidget {
               onPressed: () {
                 requestNativeBanner();
               },
-              child: Text('Request'),
+              child: Text('Request And Show'),
             ),
-            RaisedButton(
-              onPressed: () {
-                showNativeBanner();
-              },
-              child: Text('Show'),
+            Container(
+              child: Image.network("",
+                  width: 100.0, height: 100.0, fit: BoxFit.cover),
+              margin: EdgeInsets.only(top: 10.0),
+              alignment: Alignment(0.0, 0.0),
+            ),
+            new Container(
+              child: new Text(""),
+              alignment: Alignment(-1.0, 0.0),
+              margin: EdgeInsets.only(top: 10.0, left: 10.0),
             ),
           ],
         ),
@@ -40,26 +47,13 @@ class NativeBannerPage extends StatelessWidget {
         (zoneId, errorMessage) => error(zoneId, errorMessage));
   }
 
-  void showNativeBanner() {
-    TapsellPlus.showAd(Constant.TAPSELL_NATIVE_BANNER,
-        opened: (zoneId) => opened(zoneId),
-        closed: (zoneId) => closed(zoneId),
-        error: (zoneId, errorMessage) => error(zoneId, errorMessage));
-  }
-
   nativeBannerResponse(nativeBanner) {
     print("success: ad_id = ${nativeBanner.adId}");
+    adImage = nativeBanner.landscapeStaticImageUrl;
+    adTitle = nativeBanner.title;
   }
 
   error(zoneId, errorMessage) {
     print('error caught: zone_id = $zoneId, message = $errorMessage');
-  }
-
-  opened(zoneId) {
-    print("opened: zone_id = $zoneId");
-  }
-
-  closed(zoneId) {
-    print("closed: zone_id = $zoneId");
   }
 }
